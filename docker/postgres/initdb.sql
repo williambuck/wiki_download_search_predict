abf05c3dcaebcb3536aefb92f10c1eec9daed16a
joshuacook <me@joshuacook.me>
2017-08-15 15:22:45 -0700

    Update initdb.sql

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -2,3 +2,15 @@ CREATE TABLE wiki_stuff (
     id INTEGER,
     title TEXT
 );
+
+CREATE TABLE wiki_types (
+    id INTEGER,
+    type TEXT
+);
+
+
+CREATE TABLE stuff_to_type (
+    id,
+    stuff_id INTEGER,
+    type_id TEXT
+);
